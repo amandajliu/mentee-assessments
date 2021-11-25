256702a61076706f46413d4936b33cfcd0d3ab86
Amanda Jin Liu <amanda.liu@pbc.humandx.org>
2021-11-24 17:26:44 -0800

    make background color gray on click

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Add your code here! Good Luck!
 document.addEventListener("DOMContentLoaded", function(event) { 
-
-  document.getElementById("about-me").addEventListener("hover", () => { this.style.backgroundColor="red"})
-
+  document.getElementById("about-me").addEventListener("click", (event) => { event.target.style.backgroundColor="gray"})
 });
 let sum = () => {
   // hint: dont forget to return your answer in this function
